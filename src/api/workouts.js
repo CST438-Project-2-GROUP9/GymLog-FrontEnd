4520083548146e1c49622699cdbb0dff67dc404a
Aurelio-0-0 <victoperez@csumb.edu>
2026-03-25 17:43:51 -0700

    change it to render url

--- src/api/workouts.js
+++ src/api/workouts.js
@@ -1,5 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL ?? "https://gymlog-backend-5.onrender.com";
-
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 export async function getWorkouts() {
     const res = await fetch(`${API_BASE}/api/workouts`, {
         credentials: "include",
